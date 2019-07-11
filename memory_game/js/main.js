50b263792bf61222065fff15cbfb3aafd95e9857
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.hsd1.ga.comcast.net>
2019-07-11 13:25:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,17 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []
 
 var cardOne = cards[0];
-
-cardsInPlay.push('cardOne');
-
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 
 var cardTwo = cards[2];
-
-cardsInPlay.push('cardTwo');
-
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 }
